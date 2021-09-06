f539127b7e6ef2fdc4c70c2983fddc4394f9ca9e
SeoyoonPark <tjdbs118@gmail.com>
2021-09-06 22:02:56 +0900

    초기 커밋.

---
+++ BOJ/일곱난쟁이.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+
+	int height[9];
+	int sum = 0;
+
+	for (int i = 0; i < 9; i++) {
+		cin >> height[i];
+		sum += height[i];
+	}
+	int rest = sum - 100;
+	sort(height, height + 9);
+
+	for (int i = 0; i < 9 - 1; i++) {
+		for (int j = 0; j < 9; j++) {
+			if (height[i] + height[j] == rest) {
+				for (int k = 0; k < 9; k++) {
+					if (k == i || k == j) continue;
+					cout << height[k] << endl;
+				}
+				return 0;
+			}
+		}
+	}
+
+	return 0;
+}
