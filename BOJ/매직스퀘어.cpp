fd9022081417d65acc16085dbf71c0af6fcb8b35
SeoyoonPark <tjdbs118@gmail.com>
2021-09-15 21:14:45 +0900

    미완

---
+++ BOJ/매직스퀘어.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int arr[3][3];
+int cost = 0;
+
+bool square() {
+
+	for (int i = 0; i < 3; i++) {
+
+	}
+
+	return;
+}
+
+int main() {
+
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			cin >> arr[i][j];
+		}
+	}
+
+	if (square()) {
+
+	}
+	cout << cost;
+
+	return 0;
+}
