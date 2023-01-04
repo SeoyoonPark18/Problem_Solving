b548f378b5264077525f209d57ffac1154cf2edf
SeoyoonPark <tjdbs118@gmail.com>
2023-01-03 23:00:33 -0800

    11650 좌표 정렬하기

--- BOJ/11650 좌표 정렬하기.cpp
+++ BOJ/11650 좌표 정렬하기.cpp
@@ -6,30 +6,25 @@ using namespace std;
 
 int main() {
 	ios::sync_with_stdio(false);
-	cin.tie(NULL);
+	cin.tie(NULL); cout.tie(NULL);
 
 	int n; cin >> n;
 	int x, y;
 	vector<pair<int, int>> vv;
 	for (int i = 0; i < n; i++) {
+		
 		cin >> x >> y;
 		vv.push_back(make_pair(x, y));
+		//vv.emplace_back(x, y);
 	}
 
-	sort(vv.begin(), vv.end());
+	//sort(vv.begin(), vv.end(), compare); //sort by y order
+	sort(vv.begin(), vv.end()); //sort by x order
+
+
 	for (int i = 0; i < n; i++) {
-		cout << vv[i].first << " " << vv[i].second << endl;
+		cout << vv[i].first << " " << vv[i].second << "\n";
 	}
-
-	/*
-	int min = vv[0].first;
-	for (int j = 1; j < n-1; j++) {
-		int min = vv[j].first;
-		if (vv[j].first < min) {
-
-		}
-	}
-	*/
 
 	return 0;
 }
