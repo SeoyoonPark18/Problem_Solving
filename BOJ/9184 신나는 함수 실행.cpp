9d483fe78eaf235d53f4cb563a557b725e3fb339
SeoyoonPark <tjdbs118@gmail.com>
2023-01-21 02:24:23 -0800

    9184 신나는 함수 실행

--- BOJ/9184 신나는 함수 실행.cpp
+++ BOJ/9184 신나는 함수 실행.cpp
@@ -4,19 +4,26 @@
 #include <vector>
 using namespace std;
 
+int arr[21][21][21] = { 0, };
 int w(int a, int b, int c) {
 	if (a <= 0 || b <= 0 || c <= 0) {
 		return 1;
 	}
-	else if (a > 20 || b > 20 || c > 20) {
+	if (a > 20 || b > 20 || c > 20) {
 		return w(20, 20, 20);
 	}
-	else if (a < b && b < c) {
-		return w(a, b, c - 1) + w(a, b - 1, c - 1) - w(a, b - 1, c);
+
+	if (arr[a][b][c]) {
+		return arr[a][b][c];
 	}
-	else {
-		return w(a - 1, b, c) + w(a - 1, b - 1, c) + w(a - 1, b, c - 1) - w(a - 1, b - 1, c - 1);
+
+	if (a < b && b < c) {
+		arr[a][b][c] = w(a, b, c - 1) + w(a, b - 1, c - 1) - w(a, b - 1, c);
+		return arr[a][b][c];
 	}
+	arr[a][b][c] = w(a - 1, b, c) + w(a - 1, b - 1, c) + w(a - 1, b, c - 1) - w(a - 1, b - 1, c - 1);
+	return arr[a][b][c];
+
 }
 
 
@@ -24,15 +31,14 @@ int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
 
-	int a = 0; int b = 0; int c = 0;
-	while (a != -1 && b != -1 && c != -1) {
+	while (true) {
+		int a, b, c;
 		cin >> a >> b >> c;
-		cout << "w(" << a << ", " << b << ", " << c << ") = ";
-		cout << w(a, b, c) << "\n";
-	}
-	
+		if (a == -1 && b == -1 && c == -1) break;
 
-	
+		cout << "w(" << a << ", " << b << ", " << c << ") = " << w(a, b, c) << "\n";
+	}
+
 
 
 	return 0;
