8f416862a580781874cd478581c121c1c3b0302e
SeoyoonPark <tjdbs118@gmail.com>
2023-01-24 00:56:27 -0800

    1904 01타일

--- BOJ/1904 01타일.cpp
+++ BOJ/1904 01타일.cpp
@@ -2,69 +2,28 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
-#include <map>
 #include <string>
 using namespace std;
 
-vector<string> v;
-
-void tile(int n) {
-	
-	if (n == 1) {
-		v.push_back("1");
-		return;
-	}
-
-	if (n % 2 == 0) { //even number
-
-		tile(n - 1);
-
-		int len = v.size();
-		for (int i = 0; i < len; i++) {
-			string s = v[i];
-			string one = "1";
-
-			v[i] = one + s;
-			if (find(v.begin(), v.end(), s+ one) == v.end()) {
-				v.push_back(s + one);
-			}	
-		}
-	
-		string zero = "";
-		for (int i = 0; i < n; i++) {
-			zero += '0';
-		}
-		v.push_back(zero);
-
-		
-		v.erase(unique(v.begin(), v.end()), v.end());
-		
-	}
-	else { //odd number
-		tile(n - 1);
-
-		int len = v.size();
-		for (int i = 0; i < len; i++) {
-			string s = v[i];
-			string one = "1";
-
-			v[i] = one + s;
-			if (find(v.begin(), v.end(), s + one) == v.end()) {
-				v.push_back(s + one);
-			}
-		}
-		v.erase(unique(v.begin(), v.end()), v.end());
-	}
-}
+vector<int> v;
 
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
 
 	int n; cin >> n;
-	tile(n);
-	
-	cout << v.size();
+
+	v.push_back(1); //idx 0
+	v.push_back(1); //1
+	for (int i = 2; i <= n; i++) {
+		int num = v[i - 1] % 15746 + v[i - 2] % 15746;
+		v.push_back(num);
+	}
+
+	cout << v[n] % 15746;
+
+
+
 
 	return 0;
 }
