4407f722c7c8451dda5465052dd09cef70eca438
SeoyoonPark <tjdbs118@gmail.com>
2023-01-26 23:32:42 -0800

    9461 파도반 수열

--- BOJ/9461 파도반 수열.cpp
+++ BOJ/9461 파도반 수열.cpp
@@ -1,42 +1,41 @@
 //BOJ 9461
 #include <iostream>
 #include <algorithm>
-#include <vector>
-#include <string>
 using namespace std;
 
-int p(int n) {
-	if (n > 5 && n < 9) {
-		return p(n - 1) + 1;
-	}
-	else if (n > 8 && n < 11) {
-		return p(n - 1) + 2;
-	}
-	else if (n > 10) {
-		return p(n - 1) + (n - 8);
-	}
-	else if (n < 4) {
-		return 1;
-	}
-	else if (n > 3 && n < 6) {
-		return 2;
-	}
-}
+long long arr[101] = { 0, }; // int(x)
+
+
 
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
 
+	arr[1] = 1; arr[2] = 1; arr[3] = 1;
+	arr[4] = 2; arr[5] = 2;
+
+	for (int i = 6; i < 101; i++) {
+		if (i < 9) {
+			arr[i] = arr[i - 1] + 1;
+		}
+		if (i > 8 && i < 11) {
+			arr[i] = arr[i - 1] + 2;
+		}
+		if (i > 10) {
+			arr[i] = arr[i - 1] + arr[i - 5];
+		}
+	}
+
 
 	int t; cin >> t;
 	for (int i = 0; i < t; i++) {
 		int n; cin >> n;
-		cout << p(n) << "\n";
+		cout << arr[n] << "\n";
 
 	}
-	
 
-	
+
+
 
 	return 0;
 }
