ccc43c365c0beefb06f5c17cd16fc3c8622182cb
SeoyoonPark <tjdbs118@gmail.com>
2023-03-03 14:34:34 -0800

    11053 가장 긴 증가하는 부분수열

--- BOJ/11053 가장 긴 증가하는 부분 수열.cpp
+++ BOJ/11053 가장 긴 증가하는 부분 수열.cpp
@@ -9,6 +9,7 @@ int main() {
 
 	int dp[2][1001];
 	int mmax = 1;
+
 	int n; cin >> n;
 	for (int i = 1; i <= n; i++) {
 		cin >> dp[0][i];
@@ -16,8 +17,22 @@ int main() {
 
 	dp[1][1] = 1;
 	for (int i = 2; i <= n; i++) {
+		dp[1][i] = 1;
+		for (int j = 1; j < i; j++) {
+			if (dp[0][j] < dp[0][i]) {
+				dp[1][i] = max(dp[1][i], dp[1][j] + 1);
+			}
+			if (dp[1][i] > mmax)
+				mmax = dp[1][i];
+		}
+	}
+
+
+	/*
+	dp[1][1] = 1;
+	for (int i = 2; i <= n; i++) {
 		int prev = 0; int idx = 0;
-		for (int j = 1; j < i; j++) {	
+		for (int j = 1; j < i; j++) {
 			if (dp[0][j] < dp[0][i] && prev < dp[0][j]) {
 				prev = dp[0][j];
 				idx = j;
@@ -33,7 +48,8 @@ int main() {
 
 		}
 	}
-	
+	*/
+
 	cout << mmax;
 
 
