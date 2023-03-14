8a8af02261f4cf122213a93b032e71052c778c85
SeoyoonPark <tjdbs118@gmail.com>
2023-03-14 00:40:54 -0700

    시간초과

---
+++ BOJ/1931 회의실배정.cpp
@@ -0,0 +1,39 @@
+//BOJ 1931
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	
+	int n; cin >> n;
+	vector<pair<int, int>> p;
+	vector<int> v;
+
+	for (int i = 0; i < n; i++) {
+		int s, e; cin >> s >> e;
+		p.push_back({ s,e });
+	}
+	sort(p.begin(), p.end());
+
+	int last = p[n - 1].second; //14
+	
+	for (int i = 0; i < last+1; i++) {
+		v.push_back(0);
+	}
+	
+
+	int maxN = 0;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j <= p[i].first; j++) {
+			int idx = p[i].second;
+			v[idx] = max(v[idx], v[j] + 1);
+			maxN = max(maxN, v[idx]);
+		}
+	}
+	cout << maxN;
+
+	return 0;
+}
