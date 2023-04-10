8c8a8d7928aa052825dad12e868778b15e4ef855
SeoyoonPark <tjdbs118@gmail.com>
2023-04-09 23:54:18 -0700

    11054

--- BOJ/11054 가장 긴 바이토닉 부분 수열.cpp
+++ BOJ/11054 가장 긴 바이토닉 부분 수열.cpp
@@ -11,28 +11,35 @@ int main() {
 	int arr[1001] = { 0, };
 	int dpIn[1001] = { 0, };
 	int dpDe[1001] = { 0, };
-	int n; cin >> n;	
-	for (int i = 1; i <= n; i++) {
+	int n; cin >> n;
+	for (int i = 0; i < n; i++) {
 		cin >> arr[i];
 	}
 
 
-	for (int i = 1; i <= n; i++) {
-
-
-		for (int j = 1; j <= n; j++) {
-			if (j < i && arr[i] > arr[j]) {
-				dpIn[i] = max(dpIn[i], dpIn[j] + 1);
-			}
-			
-			if (j > i) {
-
+	for (int right = 0; right < n; right++) {
+		dpIn[right] = 1;
+		for (int left = 0; left < right; left++) {
+			if (arr[right] > arr[left]) {
+				dpIn[right] = max(dpIn[right], dpIn[left] + 1);
 			}
 		}
 	}
 
+	for (int left = n - 1; left >= 0; left--) {
+		dpDe[left] = 1;
+		for (int right = n - 1; left < right; right--) {
+			if (arr[left] > arr[right]) {
+				dpDe[left] = max(dpDe[left], dpDe[right] + 1);
+			}
+		}
+	}
 
-
+	int sum = 0;
+	for (int i = 0; i < n; i++) {
+		sum = max(sum, dpIn[i] + dpDe[i]);
+	}
+	cout << sum - 1;
 
 	return 0;
 }
