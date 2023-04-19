49bd42bbbf782e8b67aa8d45c316a0bbc6554f4c
SeoyoonPark <tjdbs118@gmail.com>
2023-04-19 00:03:08 -0700

    1966 프린터 큐

--- BOJ/etc/1966 프린터 큐.cpp
+++ BOJ/etc/1966 프린터 큐.cpp
@@ -5,49 +5,40 @@
 #include <queue>
 using namespace std;
 
-
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
 
-	/*
 	int test; cin >> test;
 	for (int i = 0; i < test; i++) {
-		
-		
+		queue<pair<int, int>> q;
+		vector<int> tmp;
 
-
-	}
-	*/
-
-
-	queue<pair<int, int>> q;
-	vector<int> tmp;
-
-	int n; cin >> n;
-	int m; cin >> m;
-	int cnt = 0;
-	int idx = 0;
-	for (int j = 0; j < n; j++) {
-		int value; cin >> value;
-		q.push({ j,value });
-		tmp.push_back(value);
-	}
-	sort(tmp.begin(), tmp.end(), greater<int>());
-
-	if (q.front().second != tmp[idx]) {
-		q.push({ q.front().first, q.front().second });
-		q.pop();
-	}
-	else {
-		cnt++;
-		idx++;
-		if (q.front().first == m) {
-			cout << cnt << '\n';
+		int n; cin >> n;
+		int m; cin >> m;
+		int cnt = 0;
+		for (int j = 0; j < n; j++) {
+			int value; cin >> value;
+			q.push({ j,value });
+			tmp.push_back(value);
 		}
-		q.pop();
-	}
+		sort(tmp.begin(), tmp.end(), greater<int>());
 
+		while (1) {
+			if (q.front().second != tmp[cnt]) {
+				q.push({ q.front().first, q.front().second });
+				q.pop();
+			}
+			else {
+				cnt++;
+				if (q.front().first == m) {
+					cout << cnt << '\n';
+					break;
+				}
+				q.pop();
+			}
+		}
+	}
 
 	return 0;
 }
