2f7862d8aa1d0296ab4dee426b41c5b096c1220c
SeoyoonPark <tjdbs118@gmail.com>
2023-04-25 00:34:10 -0700

    11660 구간 합 구하기5

--- BOJ/etc/11660 구간 합 구하기5.cpp
+++ BOJ/etc/11660 구간 합 구하기5.cpp
@@ -4,14 +4,16 @@
 #include <vector>
 using namespace std;
 
+int arr[1025][1025] = { 0, };
+
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
 
-	int arr[1025][1025] = { 0, };
-
+	//int arr[1025][1025] = { 0, };
 	int n; int m;
 	cin >> n >> m;
+
 	for (int i = 1; i <= n; i++) {
 		for (int j = 1; j <= n; j++) {
 			int num; cin >> num;
@@ -22,20 +24,13 @@ int main() {
 	for (int i = 0; i < m; i++) {
 		int x1, x2, y1, y2;
 		cin >> x1 >> y1 >> x2 >> y2;
-
-		int cnt = y2 - y1 + 1;
 		int sum = 0;
-		for (int j = y1; j <= y2; j++) {
-			sum += arr[j][x2] - arr[j][x1 - 1];
+		for (int j = x1; j <= x2; j++) {
+			sum += arr[j][y2] - arr[j][y1 - 1];
 		}
 
-		
 		cout << sum << '\n';
-
 	}
-
-
-
 
 	return 0;
 }
