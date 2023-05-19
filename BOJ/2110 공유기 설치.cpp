4487f1cb7fa8854fe4d471fbfdfc6be62b30ee1e
SeoyoonPark <tjdbs118@gmail.com>
2023-05-18 20:02:32 -0700

    2100 공유기 설치

--- BOJ/2110 공유기 설치.cpp
+++ BOJ/2110 공유기 설치.cpp
@@ -4,31 +4,50 @@
 #include <vector>
 using namespace std;
 
+int n, c;
+vector<long long> a;
+
+bool check(long long m) {
+	int router = a[0]; //
+	int cnt = 1;
+	for (int i = 0; i < n; i++) {
+		if (a[i] - router >= m) {
+			cnt += 1;
+			router = a[i];
+		}
+		if (cnt >= c) return true;
+	}
+	return false;
+}
+
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
 
-	vector<int> v;
-	int len = 0;
-
-	int n; int c;
 	cin >> n >> c;
 	for (int i = 0; i < n; i++) {
 		int num; cin >> num;
-		v.push_back(num);
+		a.push_back(num);
 	}
-	sort(v.begin(), v.end());
+	sort(a.begin(), a.end());
 
-	int start = 1;
-	int end = v[n - 1];
+	long long start = 1;
+	long long end = a[n - 1] - a[0];
+	long long leng = 0;
 
 	while (start <= end) {
-		int mid = (start + end) / 2;
-
-
+		long long mid = (start + end) / 2;
+		
+		if (check(mid)) {
+			leng = max(leng, mid);
+			start = mid + 1;
+		}
+		else {
+			end = mid - 1;
+		}
 	}
 
-	cout << len;
+	cout << leng;
 
 	return 0;
 }
