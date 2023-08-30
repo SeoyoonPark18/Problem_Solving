f778a213eadcc21ee50232e9523060ec2227a26f
SeoyoonPark <tjdbs118@gmail.com>
2023-08-30 19:05:24 +0900

    2206 벽 부수고 이동하기

--- BOJ/Class 4/2206 벽 부수고 이동하기.cpp
+++ BOJ/Class 4/2206 벽 부수고 이동하기.cpp
@@ -3,61 +3,44 @@
 #include <algorithm>
 #include <vector>
 #include <queue>
+#include <tuple>
 #include <string>
 using namespace std;
 
 int n, m;
 int map[1001][1001];
-bool visited[1001][1001];
-int cnt[1001][1001] = { 0, };
+int cnt[1001][1001][2];
 
 int dx[4] = { 0,0,1,-1 };
 int dy[4] = { 1,-1,0,0 };
 
-bool isRange(int x, int y) {
-	if (x > 0 && y > 0 && x <= n && y <= m) return true;
-	else return false;
-}
 
-void bfs(int x, int y) {
-	queue<pair<int,int>> q;
-	q.push(make_pair(x, y));
-	visited[x][y] = true;
-	cnt[x][y] = 1;
+void bfs() {
+	queue<tuple<int, int, int>> q;
+	q.push(make_tuple(1, 1, 1));
+	cnt[1][1][1] = 1;
 	while (!q.empty()) {
-		int a = q.front().first;
-		int b = q.front().second;
+		int a = get<0>(q.front());
+		int b = get<1>(q.front());
+		int c = get<2>(q.front()); //breakable
 		q.pop();
-		
+
 		if (a == n && b == m) {
-			cout << cnt[n][m];
+			cout << cnt[n][m][c];
 			return;
 		}
 
 		for (int i = 0; i < 4; i++) {
-			int nx = a + dx[i];
-			int ny = b + dy[i];
-
-			if (isRange(nx,ny) && !visited[nx][ny]) {
-				if (map[nx][ny] == 0) {
-					if (map[a][b] == -1) {
-						map[nx][ny] = -1;
-					}
-					q.push(make_pair(nx, ny));
-					visited[nx][ny] = true;
-					cnt[nx][ny] = cnt[a][b] + 1;
+			int ny = a + dy[i];
+			int nx = b + dx[i]; //xy order!
+			if (nx >= 1 && ny >= 1 && nx <= m && ny <= n && !cnt[ny][nx][c]) {
+				if (map[ny][nx] == 0) {
+					q.push(make_tuple(ny, nx, c));
+					cnt[ny][nx][c] = cnt[a][b][c] + 1;
 				}
-				else {
-					if (map[a][b] == -1) {
-						visited[nx][ny] = true;
-						cnt[nx][ny] = 0;
-					}
-					else {
-						map[nx][ny] = -1;
-						q.push(make_pair(nx, ny));
-						visited[nx][ny] = true;
-						cnt[nx][ny] = cnt[a][b] + 1;
-					}					
+				else if (map[ny][nx] == 1 && c == 1) {
+					q.push(make_tuple(ny, nx, c - 1));
+					cnt[ny][nx][c - 1] = cnt[a][b][c] + 1;
 				}
 			}
 		}
@@ -74,14 +57,11 @@ int main() {
 	for (int i = 1; i <= n; i++) {
 		cin >> s;
 		for (int j = 1; j <= m; j++) {
-			map[i][j] = s[j - 1] - 48;
+			map[i][j] = s[j - 1] - '0';
 		}
 	}
 
-	bfs(1, 1);
-	//if (cnt[n][m] == 0) cout << -1;
-	//else cout << cnt[n][m];
-	
+	bfs();
 
 
 	return 0;
