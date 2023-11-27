fe579df80ea43730a1a1f4d5ed96f066c16c71b1
Seoyoon Park <tjdbs118@gmail.com>
2023-11-27 17:43:28 +0900

    [level 3] Title: 단어 변환, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.cpp
@@ -0,0 +1,54 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+vector<string> v[51];
+int visited[51] = {0,};
+int ans = 100;
+
+void dfs(string cur, vector<string> words, int count, string t){
+    if(count >= ans) return;
+    if(cur == t){
+        ans = min(ans, count);
+        return;
+    }
+    else
+    {
+        for(int i=0; i<words.size();i++){
+            int cnt = 0;
+            for(int k=0; k<cur.length(); k++){
+                if(cur[k] == words[i][k]){
+                  cnt++;
+                }
+            }
+            if(cnt == cur.length()-1 && !visited[i]){
+                visited[i]++;
+                dfs(words[i], words, count+1, t);
+                visited[i]--;
+            }
+        }
+    }   
+}
+
+int solution(string begin, string target, vector<string> words) {
+    int n = begin.length();    
+
+    for(int i=0; i<words.size();i++){
+        int cnt = 0;
+        for(int k=0; k<n; k++){
+            if(begin[k] == words[i][k]){
+                cnt++;
+            }
+        }
+        if(cnt == n-1){
+            //visited[i]++;
+            dfs(words[i], words, 1, target);
+            
+        }
+    }
+ 
+    if(ans == 100) return 0;
+    return ans;
+}
